0e5ebe9415776bc81313935f20d493520b842b70
Gyuchool <balance0591@naver.com>
2020-09-02 17:00:19 +0900

    object

---
+++ syntax/object.js
@@ -0,0 +1,10 @@
+var members=['egoing', 'kim', 'hoie'];
+
+console.log(members[1]);//kim
+
+var roles={
+    'programmer':'egoing',
+    'designer':'kim',
+    'manger':'hoie'
+}
+console.log(roles.designer);//kim
