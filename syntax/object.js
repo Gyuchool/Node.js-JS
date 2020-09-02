c64ae0d8c884d04571276ed25429ddc861d9f734
Gyuchool <balance0591@naver.com>
2020-09-02 17:06:38 +0900

    object

--- syntax/object.js
+++ syntax/object.js
@@ -1,10 +1,20 @@
 var members=['egoing', 'kim', 'hoie'];
-
 console.log(members[1]);//kim
 
+var i=0;
+while(i<members.length){
+    console.log('array loop', members[i]);
+    i=i+1;
+
+}
 var roles={
     'programmer':'egoing',
     'designer':'kim',
     'manger':'hoie'
 }
 console.log(roles.designer);//kim
+console.log(roles['designer']);//kim
+
+for(var name in roles){
+    console.log('object ==> ', name, 'value ==> ', roles[name]);
+}
